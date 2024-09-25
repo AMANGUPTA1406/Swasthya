aba618f0c03bf4bf6b6acb9d7fe69db8865c9444
Aman Gupta <kumar.aman1406@gmail.com>
2024-09-25 16:45:10 +0530

    first commit

---
+++ client/src/components/Hero.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import image from "../images/heroimg.jpg";
+import "../styles/hero.css";
+
+const Hero = () => {
+  return (
+    <section className="hero">
+      <div className="hero-content">
+        <h1>
+          Your Health, <br />
+          Our Responsibility
+        </h1>
+        <p>
+        At the heart of our system is a commitment to providing better, 
+        more personalized healthcare. We strive to make the patient experience smoother, 
+        helping doctors and patients connect effortlessly for timely and compassionate care.
+        </p>
+      </div>
+      <div className="hero-img">
+        <img
+          src={image}
+          alt="hero"
+        />
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
